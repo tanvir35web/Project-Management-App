6e1b3a77d4ee3293868cc245a3f6eb48cbadfb1d
tanvir35web <islammdtanvirul@gmail.com>
2024-01-02 14:25:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectsSideBar from "./components/ProjectsSideBar";
+
+function App() {
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSideBar/>
+      <NoProjectSelected/>
+    </main>
+  );
+}
+
+export default App;
