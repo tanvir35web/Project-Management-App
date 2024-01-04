0422cbf00e973977f7f39423958e7b7560f1e8db
tanvir35web <islammdtanvirul@gmail.com>
2024-01-04 21:11:40 +0600

    MOdal style and button functional

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,10 @@
 import React, { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
-export default function NewProject({ onAdd }) {
+export default function NewProject({ onAdd, onCancel }) {
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -18,7 +21,8 @@ export default function NewProject({ onAdd }) {
       enteredDescription.trim() === "" ||
       enteredDueDate.trim() === ""
     ) {
-      // show the error modal
+      modal.current.open();
+      return;
     }
 
     onAdd({
@@ -29,31 +33,39 @@ export default function NewProject({ onAdd }) {
   }
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          {" "}
-          <button className="text-stone-800 hover:text-stone-950">
+    <>
+      <Modal ref={modal} buttonCaption="Okay">
+        <h2 className='text-xl font-bold text-stone-700 my-4'>Invalid Input</h2>
+        <p className='text-stone-700 mb-4'>Oops.. looks like you forgot to entered a value. </p>
+        <p className='text-stone-700 mb-4'>Please make sure you provide a valid value for every input field</p>
+      </Modal>
+
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
             {" "}
-            Cancel{" "}
-          </button>{" "}
-        </li>
-        <li>
-          {" "}
-          <button
-            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-            onClick={handleSave}
-          >
+            <button className="text-stone-800 hover:text-stone-950" onClick={onCancel}>
+              {" "}
+              Cancel{" "}
+            </button>{" "}
+          </li>
+          <li>
             {" "}
-            Save{" "}
-          </button>{" "}
-        </li>
-      </menu>
-      <div>
-        <Input type="text" ref={title} label="Title" />
-        <Input ref={description} label="Description" textarea />
-        <Input type="date" ref={dueDate} label="Due Date" />
+            <button
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+              onClick={handleSave}
+            >
+              {" "}
+              Save{" "}
+            </button>{" "}
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={title} label="Title" />
+          <Input ref={description} label="Description" textarea />
+          <Input type="date" ref={dueDate} label="Due Date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
