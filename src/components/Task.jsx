761809b032d53dbbbfe346ea364b45d5a0207793
tanvir35web <islammdtanvirul@gmail.com>
2024-01-05 20:23:28 +0600

    NEW task added

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import NewTask from "./NewTask";
 
 export default function Task() {
   return (
     <section>
       <h2 className="text-2xl font-bold text-stone-700 mb-4">Task</h2>
-      New task
+      <NewTask/>
       <p className="text-stone-800 mb-4">This project dose not have any task yet</p>
       <ul></ul>
     </section>
